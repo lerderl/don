ca0a2a524d0f6906e360a724c0c05d237015934a
Joseph Olukunle <josepholukunle1107@gmail.com>
2022-07-22 11:01:06 +0100

    Added the dashboard tabs

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -125,6 +125,92 @@ const Dashboard = () => {
           <div className="page-content-body" id="page-content-body">
             <h3 className="page-title">Dashboard</h3>
           </div>
+
+          <ul className="nav bg-light position-relative">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Dashboard
+              </a>
+            </li>
+            <button
+              className="btn btn-light dropdown-toggle position-absolute top-0 end-0"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Actions
+            </button>
+            <ul className="dropdown-menu dropdown-menu-end">
+              <li>
+                <a className="dropdown-item" href="#">
+                  Print
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Export to Excel
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Export to Word
+                </a>
+              </li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Save As
+                </a>
+              </li>
+            </ul>
+          </ul>
+
+          {/* <ul className="page-breadcrumb">
+            <li>
+              <i className="fa fa-home"></i>
+              <a href="index.php">Home</a>
+              <i className="fa fa-angle-right"></i>
+            </li>
+            <li>
+              <a href="#">Dashboard </a>
+            </li>
+          </ul>
+          <div className="page-toolbar">
+            <div className="btn-group pull-right">
+              <button
+                type="button"
+                className="btn btn-fit-height grey-salt dropdown-toggle hover-initialized"
+                data-toggle="dropdown"
+                data-hover="dropdown"
+                data-delay="1000"
+                data-close-others="true"
+              >
+                Actions <i className="fa fa-angle-down"></i>
+              </button>
+              <ul className="dropdown-menu pull-right" role="menu">
+                <li>
+                  <a href="#">Print</a>
+                </li>
+                <li>
+                  <a href="#">Export to Excel</a>
+                </li>
+                <li>
+                  <a href="#">Export to Word</a>
+                </li>
+                <li className="divider"></li>
+                <li>
+                  <a href="#">Save As</a>
+                </li>
+              </ul>
+            </div>
+          </div> */}
         </div>
       </div>
     </>
